9d36f81096aa37332cf0bb3e6afa762eeb49da7c
nikki <rk_787@hotmail.co.uk>
2020-10-24 01:01:55 +0100

    HTTPS And data changed

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     //Connect API key
     function getCityWeather(city){
     $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=84d8ea5be6d3527e97c1d32d85112fb7",
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=84d8ea5be6d3527e97c1d32d85112fb7",
        type: "GET",
        dataType: "json",
        success: function(data){
@@ -19,9 +19,9 @@ $(document).ready(function() {
            let cityTemp = document.getElementById("city-temp");
            cityTemp.textContent = data.main.temp;
            let cityHumidity = document.getElementById("city-humidity");
-           cityHumidity.textContent = main.humidity;
+           cityHumidity.textContent = data.main.humidity;
            let cityWindSpeed = document.getElementById("city-windspeed");
-           cityWindSpeed.textContent = wind.speed;
+           cityWindSpeed.textContent = data.wind.speed;
            //temp data.main.temp
            //wind speed 
            //humidity
@@ -44,8 +44,8 @@ $(document).ready(function() {
                let forecast4 = document.getElementById("forecast4"); 
                let forecast5 = document.getElementById("forecast5");
                data.list[1].data.main.temp
-               data.list[2].main.humidity
-               data.list[3].wind.speed
+               data.list[2].data.main.humidity
+               data.list[3].data.wind.speed
                
 
               
@@ -55,7 +55,7 @@ $(document).ready(function() {
     }
 
     function getUVI(lat, lon){
-        let queryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=84d8ea5be6d3527e97c1d32d85112fb7"
+        let queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=84d8ea5be6d3527e97c1d32d85112fb7"
         $.ajax({
             url: queryURL,
            type: "GET",
@@ -70,11 +70,9 @@ $(document).ready(function() {
     }
 
     function populateHistorySection(city){
+
     
-// what we were trying to do is get whatevers in local storage and add the new city searched to it
-// then put it back into storage
-// then loop over the list of cities in local storage and create li elements for each 
-// and append them to the ul (unordered list) in the html page
+    
 
 
 
