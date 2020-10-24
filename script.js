e04e171abcf8558f6b77af9ae45d87524388366a
nikki <rk_787@hotmail.co.uk>
2020-10-25 00:37:25 +0100

    changes

--- script.js
+++ script.js
@@ -80,6 +80,25 @@ $(document).ready(function() {
                console.log(data);
                let UV = document.getElementById("UV"); 
            UV.textContent = "UV Index: " + data.value;
+           $.get(queryURL).then(function(response){
+            for (i=0;i<5;i++){
+                var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+                var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
+                var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+                var tempK= response.list[((i+1)*8)-1].main.temp;
+                var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
+                var humidity= response.list[((i+1)*8)-1].main.humidity;
+            
+                $("#fDate"+i).html(date);
+                $("#fImg"+i).html("<img src="+iconurl+">");
+                $("#fTemp"+i).html(tempF+"&#8457");
+                $("#fHumidity"+i).html(humidity+"%");
+            }
+            
+        });
+    }
+    
+    
 
            
 
@@ -100,9 +119,8 @@ $(document).ready(function() {
 
 
 
+ }})}
 
-           }})}
 
-
-        })
+})
         
